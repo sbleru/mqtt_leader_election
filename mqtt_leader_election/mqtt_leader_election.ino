1a9faa43a672bad16d6c55fc07b115751d585ca2
丸山真治 <usareru@gmail.com>
2016-10-13 21:46:06 +0900

    make leader election but not testing

--- mqtt_leader_election/mqtt_leader_election.ino
+++ mqtt_leader_election/mqtt_leader_election.ino
@@ -25,6 +25,7 @@ EthernetClient ethClient;
 char ipTopic[] = "ip";
 char rttTopic[15];  //各端末のipアドレスとする
 char electTopic[] = "election";
+char getIDTopic[] = "getID";
 char dataTopic[] = "data";
 char syncTopic[] = "sync";
 char setTopic[] = "set";
@@ -52,8 +53,8 @@ char message_buff[100];
 #define LEADER    1
 
 //first role is follower
-//int role = FOLLOWER;
-int role = LEADER;
+int role = FOLLOWER;
+//int role = LEADER;
 
 //リーダーを決めるための仮の代表者かどうか
 bool isPreLeader;
@@ -62,7 +63,7 @@ bool isElected = false;
 
 //プレイ端末のID
 //TODO:IPアドレスとかどういった者にするべきか検討
-int deviceID;
+int deviceID=-1;
 
 class Player{
   int playerID;
@@ -77,16 +78,21 @@ void Player::setValue(int _id, int _val){
   value = _val;  
 }
 
-Player pl[3];
+Player pl[10];
 
 /*Leader Election関係*/
 unsigned long time;
 unsigned long pubTime=0;
 unsigned long rttCounter=0, rtt=0, rttAverage=0;
-char ip[15];
-char iprtt[100];
-char* eachIP[10], eachRTT[10];
-int joinCount=0;
+//char ip[15];
+int ip;
+//char iprtt[100];
+byte iprtt[2] = {0}; //{ip, rtt}
+byte eachIP[10], eachRTT[10];
+//char* eachIP[10], eachRTT[10];
+byte joinCount=0;
+byte idCount[1]={0};
+bool isAppriedID=false;
 
 //プロトタイプ宣言
 void callback(char* topic, byte* payload, unsigned int length);
@@ -98,12 +104,6 @@ void setup()
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
 
-  //initialize device ID
-  //TODO:deviceIDが0ならhost, それ以外はhostからもらうように
-  //hostにするかどうかはRTTをブリーアルゴリズムのように比較する
-  deviceID = 0;
-//  deviceID = 1;
-//  deviceID = 2;
   // init serial link for debugging
   Serial.begin(9600);
   
@@ -131,12 +131,8 @@ void loop()
       if(role == LEADER)
         client.subscribe(syncTopic, 1);
       client.subscribe(setTopic, 1);
-//      client.subscribe(electTopic, 1);
+      client.subscribe(electTopic, 1);
   }
-  
-//  if(role == LEADER){
-//    senseMode = 2;
-//  }
 
   //RTTフェーズ
   //TODO:1秒に1回にしているが検討
@@ -146,42 +142,52 @@ void loop()
     client.publish(rttTopic, "", true);
   }
 
-  //ipアドレスを各ノードに送る
-  client.publish(ipTopic, iprtt, sizeof(iprtt) / sizeof(iprtt[0]));
+  if(!isElected){
+    //ipアドレスとRTTを各ノードに送る
+    client.publish(ipTopic, iprtt, sizeof(iprtt) / sizeof(iprtt[0]));
+  } else {
+    /*もらってなければ一意のIDをもらう*/
+    if(deviceID < 0) {
+      if(role == LEADER){
+        deviceID = 0;
+      } else if(role == FOLLOWER) {
+        client.publish(getIDTopic, "apply", true);
+      }
+    }
 
-  //自身の値を取得
-  //read from light sensor (photocell)
-  int lightRead = analogRead(lightPinIn);
-  
-  //自分の値をホストへ送信
-  //publish light reading every 1 seconds
-  if (millis() > (time + 1000)) {
-    time = millis();
-    String pubString = "";
-    pubString += String(deviceID);
-    pubString += " ";
-    pubString += String(lightRead);
-    pubString.toCharArray(message_buff, pubString.length()+1);
-    client.publish(syncTopic, message_buff, true );
+    //自身の値を取得
+    //read from light sensor (photocell)
+    int lightRead = analogRead(lightPinIn);
+    
+    //自分の値をホストへ送信
+    //publish light reading every 1 seconds
+    if (millis() > (time + 1000)) {
+      time = millis();
+      String pubString = "";
+      pubString += String(deviceID);
+      pubString += " ";
+      pubString += String(lightRead);
+      pubString.toCharArray(message_buff, pubString.length()+1);
+      client.publish(syncTopic, message_buff, true );
+    }
+    
+    if (pl[0].getValue() > 500) {
+      digitalWrite(ledPin0, LOW);
+    } else {
+      digitalWrite(ledPin0, HIGH);
+    }
+    if (pl[1].getValue() > 500) {
+      digitalWrite(ledPin1, LOW);
+    } else {
+      digitalWrite(ledPin1, HIGH);
+    }
+    if (pl[2].getValue() > 500) {
+      digitalWrite(ledPin2, LOW);
+    } else {
+      digitalWrite(ledPin2, HIGH);
+    }
+    
   }
-  
-  //
-  if (pl[0].getValue() > 500) {
-    digitalWrite(ledPin0, LOW);
-  } else {
-    digitalWrite(ledPin0, HIGH);
-  }
-  if (pl[1].getValue() > 500) {
-    digitalWrite(ledPin1, LOW);
-  } else {
-    digitalWrite(ledPin1, HIGH);
-  }
-  if (pl[2].getValue() > 500) {
-    digitalWrite(ledPin2, LOW);
-  } else {
-    digitalWrite(ledPin2, HIGH);
-  }
-  
   // MQTT client loop processing
   client.loop();
 }
@@ -216,19 +222,62 @@ void setValue(char* values, unsigned int len){
   return;
 }
 
-void DecideLeader(char* _iprtt, unsigned int _len){
-  char *tok;
-  char _canIP[15], _canRTT[10];
+//void DecideLeader(char* _iprtt, unsigned int _len){
+void DecideLeader(byte* _iprtt, unsigned int _len){
+  byte tempIP, tempRTT;
+  bool isInIP = false;
+  byte minIP, minRTT;
+  char leaderIP[15];
 
-  tok = strtok(_iprtt, " "));
-  while( tok != NULL ){
-    _eachIP[joinCount] = (char *)malloc(sizeof(char) * (strlen(tok)+1));
-    strcpy(eachIP[joinCount], tok);
-    tok = strtok( NULL, " " );  /* 2回目以降 */
-    if(tok != NULL)
-      eachRTT[joinCount] = (char *)malloc(sizeof(char) * (strlen(tok)+1));
-    strcpy(eachRTT[joinCount], tok);
+  for(byte i=0; i<joinCount; i++){
+    if(eachIP[i] == _iprtt[0]){
+      //すでにIPアドレスが登録されていればRTTの更新
+      eachRTT[i] = _iprtt[1];
+      isInIP = true;
+    }
   }
+  if(isInIP){
+    //
+  } else {
+    eachIP[joinCount] = _iprtt[0];
+    eachRTT[joinCount++] = _iprtt[1];
+  }
+
+  /*２端末未満なら返す*/
+  if(joinCount < 1)
+    return;
+  /*最小ip端末を見つける*/
+  minIP = 0;
+  for(byte i=1; i<joinCount; i++){
+    if(eachIP[minIP] > eachIP[i])
+      minIP = i;
+  }
+  /*自分が最小のipアドレスでなかったら返す*/
+  if(iprtt[0] != eachIP[minIP])
+    return;
+  /*最小RTT端末を見つける*/
+  minRTT=0;
+  for(byte i=0; i<joinCount; i++){
+    if(eachRTT[minRTT] > eachRTT[i])
+      minRTT = i;
+  }
+
+  /*IPアドレスがeachIP[minRTT]であるものがLeader*/
+  sprintf(leaderIP, "%d", eachIP[minRTT]);
+  client.publish(electTopic, leaderIP, true);
+  
+//  char *tok;
+//  char _canIP[15], _canRTT[10];
+//
+//  tok = strtok(_iprtt, " ");
+//  while( tok != NULL ){
+//    _canIP[joinCount] = (char *)malloc(sizeof(char) * (strlen(tok)+1));
+//    strcpy(eachIP[joinCount], tok);
+//    tok = strtok( NULL, " " );  /* 2回目以降 */
+//    if(tok != NULL)
+//      _canRTT[joinCount] = (char *)malloc(sizeof(char) * (strlen(tok)+1));
+//    strcpy(eachRTT[joinCount], tok);
+//  }
 }
 
 // handles message arrived on subscribed topic(s)
@@ -248,8 +297,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   //Topicの場合分け
   if(strcmp(topic, ipTopic) == 0){
-    Serial.println(_recvMessage);
-    DecideLeader(_recvMessage, length);
+    Serial.println(payload[0]);
+    Serial.println(payload[1]);
+    DecideLeader(payload, length);
     
   } else if(strcmp(topic, rttTopic) == 0){
     /*rttの今までの平均を求める*/
@@ -257,12 +307,35 @@ void callback(char* topic, byte* payload, unsigned int length) {
     rttCounter++;
     rttAverage = rtt / rttCounter;
     /*iprtt = "ipAdress + " " + rtt" とする*/
-    sprintf(_rttChar, "%d", rttAverage); //数値を文字列に変換
-    strcat(_iprttTemp, ip);
-    strcat(_iprttTemp, " ");
-    strcat(_iprttTemp,_rttChar);
-    strcpy(iprtt, _iprttTemp);
+    iprtt[1] = rttAverage;
     
+//    sprintf(_rttChar, "%d", rttAverage); //数値を文字列に変換
+//    strcat(_iprttTemp, ip);
+//    strcat(_iprttTemp, " ");
+//    strcat(_iprttTemp,_rttChar);
+//    strcpy(iprtt, _iprttTemp);
+
+  } else if(strcmp(topic, electTopic) == 0){
+    if(iprtt[0] == atoi(_recvMessage)){
+      role = LEADER;
+      isElected = true;
+    } else {
+      role = FOLLOWER;
+    }
+
+  /*可動物に一意にIDを割り当てる host->guestにIDを渡す*/
+  } else if(strcmp(topic, getIDTopic) == 0){
+    if(role == LEADER){
+      if(strcmp(_recvMessage, "apply")){
+        client.publish(getIDTopic, idCount, sizeof(idCount) / sizeof(idCount[0]));
+        idCount[0]++;
+      }
+    } else if(role == FOLLOWER){
+      if(strcmp(_recvMessage, "apply"))
+        return;  
+      deviceID = atoi(_recvMessage);
+    }
+
   //TODO:メッセージの分解をホストとゲストどちらでやるか
   } else if(strcmp(topic, syncTopic) == 0){
     client.publish(setTopic, _recvMessage, true );
@@ -272,18 +345,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 }
 
+//ipアドレスを文字列としてつなげ、数値変換
 void setIPAddress()
 {
   char tempip[10];
+  char ipChar[15];
 
 //  sprintf(rttTopic, "%d", Ethernet.localIP()[3]);
 
   //ipアドレスを文字列として連結させる
   for(int i=0; i<4; i++){
     sprintf(tempip, "%d", Ethernet.localIP()[i]);
-    strcat(ip, tempip);
+    strcat(ipChar, tempip);
   }
+  strcpy(rttTopic, ipChar);
+  ip = atoi(ipChar);
+  iprtt[0] = ip;
   Serial.println(ip);
-  strcpy(rttTopic, ip);
 }
 
